95619ad292ab45662c74b3a72eff014d667d5f5d
Troy <eeohjeeki@gmail.com>
2018-03-23 02:30:27 -0500

    Updated file

--- piglatin/Program.cs
+++ piglatin/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace piglatin
+
+
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        //Creating final list of words
+        string pigLatin = String.Empty;
+
+        //User Prompt
+        Console.WriteLine("Please enter a line of text");
+        string testWord = Console.ReadLine();
+
+        //Turn sentence into individual words
+
+
+        string[] words = testWord.Split(null);
+
+
+
+
+
+
+        //Convert Each word into it's pig latin form
+        foreach (string singleWord in words)
         {
-            Console.WriteLine("Hello World!");
+            string word = "";
+            char temp = singleWord[0];
+            if (singleWord != "!")
+            {
+                word = singleWord + temp + "ay";
+            }
+            else
+            {
+                word = singleWord + temp;
+            }
+            string finalWord = word.Substring(1);
+            pigLatin = pigLatin + finalWord + " ";
+
         }
+        Console.WriteLine(pigLatin);
+        Console.ReadLine();
     }
 }
